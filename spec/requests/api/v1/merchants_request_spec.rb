897302eea4f1f0d6e803d5949c2d594acac39e04
Bryan Flanagan <bflanagan78@yahoo.com>
2023-02-07 14:46:52 -0700

    feat: Add merchants show, spec tests and postman tests passing

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -18,4 +18,18 @@ describe "merchants API" do
       expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
+
+  it 'returns a single merchant by its id' do
+    
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+    merchant = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
+
+    expect(merchant[:data]).to have_key(:id)
+    expect(merchant[:data][:id].to_i).to be_an(Integer)
+    expect(merchant[:data][:attributes]).to have_key(:name)
+    expect(merchant[:data][:attributes][:name]).to be_a(String)
+  end
 end
