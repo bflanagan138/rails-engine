1b03eddfd793af84f70a8696c4d149baecec1e21
Bryan Flanagan <bflanagan78@yahoo.com>
2023-02-07 12:00:10 -0700

    refactor: Create Merchants serializer, refactor merchants index

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -9,13 +9,13 @@ describe "merchants API" do
     expect(response).to be_successful
 
     merchants = JSON.parse(response.body, symbolize_names: true)
-    expect(merchants.count).to eq(6)
-
-    merchants.each do |merchant|
+    expect(merchants[:data].count).to eq(6)
+    merchants[:data].each do |merchant|
+      
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a(String)
+      expect(merchant[:id].to_i).to be_an(Integer)
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
 end
