a2789c8890af1cf537bd2ca0f11dd51d683d2852
Bryan Flanagan <bflanagan78@yahoo.com>
2023-02-08 20:37:39 -0700

    refactor: update MerchantItems index to include sad paths

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::MerchantItemsController < ApplicationController
   def index
-    merchant = Merchant.find(params[:merchant_id])
-    render json: merchant.items
+    if Merchant.exists?(params[:merchant_id])
+      merchant = Merchant.find(params[:merchant_id])
+      # require 'pry'; binding.pry
+      render json: ItemSerializer.new(merchant.items)
+    else
+      render json: { errors: "Error: merchant does not exist" }, status: 404
+    end
   end
 end
