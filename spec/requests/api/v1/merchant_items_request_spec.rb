549e147b79340c6ac3e875fb165f178ed479266c
Bryan Flanagan <bflanagan78@yahoo.com>
2023-02-09 14:33:42 -0700

    refactor: Add merchant item error serializer and refactor merchant items index

--- spec/requests/api/v1/merchant_items_request_spec.rb
+++ spec/requests/api/v1/merchant_items_request_spec.rb
@@ -3,6 +3,15 @@ require 'rails_helper'
 describe "merchant items" do
 
   it 'returns all items for a merchant id' do
+    
+    get "/api/v1/merchants/1/items"
+    # merchant1 = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to_not be_successful
+    expect(response.status).to eq(404)
+    data = JSON.parse(response.body, symbolize_names: true)
+    expect(data[:errors]).to be_a(Array)
+    expect(data[:errors].first[:status]).to eq("404")
+    expect(data[:errors].first[:title]).to eq ("Couldn't find Merchant with 'id'=1")
     merchant1 = create(:merchant)
     merchant2 = create(:merchant)
 
