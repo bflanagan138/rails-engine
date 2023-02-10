0a824026c0b4251633a8b26974218c470cd3631d
Bryan Flanagan <bflanagan78@yahoo.com>
2023-02-09 17:57:57 -0700

    feat: Adds non restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: :merchant_items
       end
       resources :items
+      get '/items/find', to: 'item#find'
+      get '/items/find_all', to: 'items#find'
+      get '/merchants/find', to: 'merchant#find'
+      get '/merchants/find_all', to: 'merchant#find_all'
     end
   end
 end
